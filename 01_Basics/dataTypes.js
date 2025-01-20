5b10e1a50d9852b55d481db6e2b507635aaa1148
Rishika Pawar <122422977+rishikapawar4@users.noreply.github.com>
2025-01-20 06:09:09 +0000

    conversion of datatypes

---
+++ 01_Basics/dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3)  -- we are using nodejs, not browser
+
+console.log(3+3)
+
+//Convertion
+
+let score=33
+console.log(typeof score)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn=1
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"hitesh" => true 
+
+let someNumber =33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+
+
+
+
---
+++ 01_Basics/variables.js
@@ -0,0 +1,6 @@
+const accountId = 14453
+let accountEmail="a@gmail.com"
+var accountPassword="12321"  //can reassign
+
+acountId=2
+console.log(accountId);
