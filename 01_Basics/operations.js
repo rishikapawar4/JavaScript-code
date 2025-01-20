bc6290b2948356176527a9823108750d7e656b52
Rishika Pawar <122422977+rishikapawar4@users.noreply.github.com>
2025-01-20 06:52:09 +0000

    operation

---
+++ 01_Basics/operations.js
@@ -0,0 +1,26 @@
+let value = 3
+let negValue= -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+// console.log(2/2);
+
+let str1="hello "
+let str2="world"
+
+let str3=str1+str2
+
+console.log(str3);
+console.log  
+
+let counter=100
+counter++
+console.log(counter);
+++counter
+console.log(counter);
+counter--
+console.log(counter); 
